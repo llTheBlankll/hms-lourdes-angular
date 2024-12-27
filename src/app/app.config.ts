be91f8955fcdfedc3a03b00a8d4ed4e521914ec5
Vince Angelo Olarte Batecan <Vince Angelo Olarte Batecan>
2024-12-27 20:21:29 +0800

    Initial Commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection, inject} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+import {environment} from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes), provideHttpClient(), provideApollo(() => {
+      const httpLink = inject(HttpLink);
+
+      return {
+        link: httpLink.create({
+          uri: environment.graphql.uri,
+        }),
+        cache: new InMemoryCache(),
+      };
+    })
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  graphql: {
+    uri: 'http://localhost:8080/graphql',
+    cache: true
+  }
+};
