237ba1cb68a2ffbec012a0eea9903594d096122a
Vince Angelo Olarte Batecan <Vince Angelo Olarte Batecan>
2024-12-28 11:55:04 +0800

    feat: configure PrimeNG and update application routes
    Added PrimeNG configuration to app.config.ts, including theme and CSS layer settings. Updated app.routes.ts to include login route and default redirect to login page. Configured providePrimeNG and provideAnimationsAsync in app.config.ts to enable PrimeNG features. Updated providers in app.config.ts to include PrimeNG and animation configurations.
    
    Fixes issue where application routes were not properly configured, and PrimeNG was not enabled.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,32 @@
-import {ApplicationConfig, provideZoneChangeDetection, inject} from '@angular/core';
+import {ApplicationConfig, inject, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideApollo } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
+import {provideHttpClient} from '@angular/common/http';
+import {provideApollo} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
 import {environment} from '../environments/environment';
+import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import Aura from '@primeng/themes/aura'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false,
+          cssLayer: {
+            name: "primeng",
+            order: "tailwind-base, primeng, tailwind-utilities"
+          }
+        }
+      }
+    }),
+    provideAnimationsAsync(),
     provideRouter(routes), provideHttpClient(), provideApollo(() => {
       const httpLink = inject(HttpLink);
 
